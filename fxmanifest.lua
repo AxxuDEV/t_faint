1ea8604d7f1695ab71d29f4a5e57a998cfb15f3a
AxxuDEV <134137941+AxxuDEV@users.noreply.github.com>
2023-05-20 13:37:05 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 author 'Taikaukk0#3313'
 description 'Tosi crazy raakakoodi221423'
 client_scripts{'**/cl_*.lua'}
+
+shared_script '@ox_lib/init.lua'
